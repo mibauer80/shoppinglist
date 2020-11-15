0ea0424888548294ddf75c788c3cee489358318a
cloudmiba <62515944+cloudmiba@users.noreply.github.com>
2020-11-15 19:13:38 +0100

    nachricht

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,47 @@
   <div>
     <h1>This is an about page</h1>
     
-    <v-combobox v-if="item_names" :items="item_names" :search-input.sync="searchInput" v-model="productNameInput"></v-combobox>
-    <v-btn
+    <v-combobox v-if="item_names" :items="item_names" v-model="productNameInput"></v-combobox>
+
+
+      <v-item-group
+      v-if="pos"
+        v-model="posSelect"
+      >
+              <v-row>
+          <v-col
+            v-for="(p, i) in pos"
+            :key="i"
+            cols="6"
+            sm="3"
+            
+          >
+      <v-item v-slot="{ active, toggle }" >
+              <v-img 
+                :src="require('../assets/logo_pos_' + p.id + '.svg')"
+                max-height="100"               
+
+
+                
+                :class="{ 'faded': !active }"              
+                @click="toggle"
+              >
+              </v-img>
+    </v-item>
+          </v-col>
+          </v-row>
+      </v-item-group>
+
+
+
+    <v-btn v-if="item_names"
   elevation="2"
   x-large
   @click="addItem(productNameInput, 1, 1)"
 >Eintragen</v-btn>
       <v-alert v-for="message in messages" :key="message.ts" v-id="message.ts"
-  dismissible
-  @click="deleteMessage(message.ts)"
-  type="warning">
+  :type="message.type"
+  @click="deleteMessage(message.ts)">
   <span v-for="n in message.messages" :key="n.index">
     {{ n }}
   </span>
@@ -19,6 +50,10 @@
   </v-alert>
   </div>
 </template>
+<style>
+.faded {opacity:0.35}
+.v-item-group .v-image__image {background-size:auto;}
+</style>
 
 <script>
   import {
@@ -27,7 +62,7 @@
 
   export default {       
     name: 'About',
-    computed: mapGetters(['items', 'item_names', 'messages']),
+    computed: mapGetters(['items', 'item_names', 'messages', 'pos']),
     methods: {
       addItem(productName, quantity, posId) {        
         this.$store.dispatch('addItem', {
