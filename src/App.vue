09ee11af3691f9f1753ac52595fa7fa2aa6ba368
cloudmiba <62515944+cloudmiba@users.noreply.github.com>
2020-11-19 21:49:00 +0100

    cart add

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
       </div>
-      
-<v-btn to="/">Home</v-btn>
-<v-btn to="/about">About</v-btn>
+
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/about">About</v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-          <v-container fluid>
-     
-      <router-view></router-view>
+      <v-container>
 
-    </v-container>
+        <router-view></router-view>
+
+      </v-container>
     </v-main>
-    
+
   </v-app>
 </template>
 
 <script>
-
-import { mapGetters } from 'vuex';
+  import {
+    mapGetters
+  } from 'vuex';
   export default {
     name: 'App',
     created() {
-    this.$store.dispatch('getProducts');
-    this.$store.dispatch('getItems');
-    this.$store.dispatch('getCategories');
-    this.$store.dispatch('getPos');
-  },
-  data () {
-    return {
-      alert: false,
+      this.$store.dispatch('getProducts');
+      this.$store.dispatch('getItems');
+      this.$store.dispatch('getCategories');
+      this.$store.dispatch('getPos');
+    },
+    data() {
+      return {
+        alert: false,
+      }
+    },
+    computed: mapGetters(['items']),
+    methods: {
+
+
     }
-  },
-computed: mapGetters(['items']),
-methods: {
-    /*hide_alert: function () {
-      console.log('Hide')
-      // `event` is the native DOM event
-      window.setInterval(() => {
-        this.alert = false;
-        console.log("hide alert after 5 seconds");
-      }, 5000)    
-    }*/
-  },
-  /*mounted: function () {
-    if(alert){
-      this.hide_alert();
-    }
-  }*/
   }
 </script>
